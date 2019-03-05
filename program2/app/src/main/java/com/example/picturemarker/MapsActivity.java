f8a69e0cd840716270bc43d4caf202f6ec8e9f49
jclaytor2010 <jclaytor@uwyo.edu>
2019-03-04 21:17:56 -0700

    Basic Project
    Got image taking and marker placement working. Just need to figure out locations.

--- program2/app/src/main/java/com/example/picturemarker/MapsActivity.java
+++ program2/app/src/main/java/com/example/picturemarker/MapsActivity.java
@@ -1,18 +1,48 @@
 package com.example.picturemarker;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.location.Location;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+    private static final int TAKE_PICTURE = 1;
     private GoogleMap mMap;
+    private FloatingActionButton button;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private SettingsClient settingsClient;
+    ImageView test;
+
+
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +52,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+
+
+        test = findViewById(R.id.imageView);
+        button = findViewById(R.id.picture);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pictureIntent = new Intent(
+                        MediaStore.ACTION_IMAGE_CAPTURE
+                );
+                if(pictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(pictureIntent,
+                            TAKE_PICTURE);
+                }
+            }
+        });
     }
 
 
@@ -36,11 +86,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        // Test for LARAMIE
+        LatLng LARAMIE = new LatLng(41.3114d, -105.5911d);
+        googleMap.addMarker(new MarkerOptions().position(LARAMIE));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(LARAMIE));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PICTURE &&
+                resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+
+                // Get image
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                test.setImageBitmap(Rotate(imageBitmap, 90));
+
+                // Get location
+
+            }
+        }
+    }
+
+    public Bitmap Rotate(Bitmap original, float degree){
+        Bitmap bOutput;
+        float degrees = degree;
+        Matrix matrix = new Matrix();
+        matrix.setRotate(degrees);
+        bOutput = Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), matrix, true);
+        return bOutput;
     }
 }
