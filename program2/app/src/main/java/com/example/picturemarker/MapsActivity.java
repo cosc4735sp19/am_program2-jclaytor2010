5643c95a110804ecc513d7d239cf56f090a1ffd4
Jacob Claytor <jclaytor@uwyo.edu>
2019-03-05 18:25:36 -0700

    I think it is working

--- program2/app/src/main/java/com/example/picturemarker/MapsActivity.java
+++ program2/app/src/main/java/com/example/picturemarker/MapsActivity.java
@@ -1,23 +1,38 @@
 package com.example.picturemarker;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.graphics.Matrix;
 import android.location.Location;
+import android.os.Looper;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -29,26 +44,39 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
+    public static final int REQUEST_ACCESS_startLocationUpdates = 0;
+    public static final int REQUEST_ACCESS_onConnected = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
     private static final int TAKE_PICTURE = 1;
     private GoogleMap mMap;
     private FloatingActionButton button;
+    private Button closeButton;
+    private ImageView test;
     FusedLocationProviderClient fusedLocationProviderClient;
     private SettingsClient settingsClient;
-    ImageView test;
-
-
-    private LocationSettingsRequest mLocationSettingsRequest;
-    private LocationCallback mLocationCallback;
+    private SettingsClient mSettingsClient;
+    /**
+     * Stores the types of location services the client is interested in using. Used for checking
+     * settings to determine if the device has optimal location settings.
+     */
+    private LocationSettingsRequest locationSettingsRequest;
+    /**
+     * Callback for Location events.
+     */
+    private LocationCallback locationCallback;
+    Location lastLocation;
+    LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -58,12 +86,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         settingsClient = LocationServices.getSettingsClient(this);
 
 
+        closeButton = findViewById(R.id.close);
+        closeButton.setVisibility(View.INVISIBLE);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeButton.setVisibility(View.INVISIBLE);
+                test.setVisibility(View.INVISIBLE);
+            }
+        });
 
         test = findViewById(R.id.imageView);
+        test.setVisibility(View.INVISIBLE);
+
         button = findViewById(R.id.picture);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getLastLocation();
                 Intent pictureIntent = new Intent(
                         MediaStore.ACTION_IMAGE_CAPTURE
                 );
@@ -73,9 +113,139 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         });
+
+        createLocationRequest();
+        createLocationCallback();
+        buildLocationSettingsRequest();
+
+        getLastLocation();
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    //This shows how to get a "one off" location.  instead of using the location updates
+    //
+    public void getLastLocation() {
+        //first check to see if I have permissions (marshmallow) if I don't then ask, otherwise start up the demo.
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            //I'm on not explaining why, just asking for permission.
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MapsActivity.REQUEST_ACCESS_onConnected);
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location == null) {
+
+                            return;
+                        }
+                        lastLocation = location;
+
+
+                    }
+                });
+    }
+
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        boolean coarse = false, fine = false;
+
+        //received result for GPS access
+        for (int i = 0; i < grantResults.length; i++) {
+            if ((permissions[i].compareTo(Manifest.permission.ACCESS_COARSE_LOCATION) == 0) &&
+                    (grantResults[i] == PackageManager.PERMISSION_GRANTED))
+                coarse = true;
+            else if ((permissions[i].compareTo(Manifest.permission.ACCESS_FINE_LOCATION) == 0) &&
+                    (grantResults[i] == PackageManager.PERMISSION_GRANTED))
+                fine = true;
+        }
+        // If request is cancelled, the result arrays are empty.
+        if (coarse && fine) {
+            // permission was granted
+
+            if (requestCode == REQUEST_ACCESS_startLocationUpdates) {
+                startLocationUpdates();
+            } else if (requestCode == REQUEST_ACCESS_onConnected) {
+                getLastLocation();
+            }
+
+        } else {
+            // permission denied,    Disable this feature or close the app.
+
+            finish();
+        }
+
+
+    }
+
+    protected void startLocationUpdates() {
+        //first check to see if I have permissions (marshmallow) if I don't then ask, otherwise start up the demo.
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            //I'm on not explaining why, just asking for permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MapsActivity.REQUEST_ACCESS_startLocationUpdates);
+            return;
+        }
+
+        // Begin by checking if the device has the necessary location settings.
+        mSettingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+
+                        //noinspection MissingPermission
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                                locationCallback, Looper.myLooper());
+
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+
+
+                        }
+
+
+                    }
+                });
+
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -92,7 +262,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                // Display image again.
+                Bitmap image = (Bitmap)marker.getTag();
+                test.setImageBitmap(image);
+                test.setVisibility(View.VISIBLE);
+                closeButton.setVisibility(View.VISIBLE);
                 return false;
             }
         });
@@ -108,14 +281,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 // Get image
                 Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
                 imageBitmap = Rotate(imageBitmap, 90);
-                test.setImageBitmap(imageBitmap);
+                //test.setImageBitmap(imageBitmap);
 
 
 
                 // Get location
-                //LatLng current = new LatLng(fusedLocationProviderClient.getLastLocation().getResult().getLongitude(), fusedLocationProviderClient.getLastLocation().getResult().getLatitude());
-                //mMap.addMarker(new MarkerOptions().position(current).icon(BitmapDescriptorFactory.fromBitmap(imageBitmap)));
-                //mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+                //LatLng current = new LatLng(41.3114, -105.5911);
+                LatLng current = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+
+                Marker currentMarker = mMap.addMarker(new MarkerOptions().position(current).icon(BitmapDescriptorFactory.fromBitmap(imageBitmap)));
+                currentMarker.setTag(imageBitmap);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
 
             }
         }
@@ -128,5 +304,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         matrix.setRotate(degrees);
         bOutput = Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), matrix, true);
         return bOutput;
+    }
+
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                lastLocation = locationResult.getLastLocation();
+            }
+        };
     }
 }
